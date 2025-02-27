41e4a69a862c3783caaddc4017e32c6dcc5163b7
qiankun <526472759@qq.com>
2025-02-27 17:10:43 +0800

    修改接口监听controller

--- electron/main/db/controller/user.ts
+++ electron/main/db/controller/user.ts
@@ -1,38 +1,39 @@
-import { users } from '../schema'
 import { userServices } from '../services/user'
-export class userController {
-  /**
-   * 更新或新增用户
-   */
-  static async addOrUpdate(data: any) {
-    let res
-    if (data.id) {
-      res = await userServices.updateUserById(data.id, data)
-    } else {
-      const newData = {...data}
-      delete newData.id
-      res = await userServices.insertUser(newData)
-    }
-    return res
-  }
-    /**
-   * 根据用户id获取详情
-   */
-    static async getInfoById({id}) {
-      return await userServices.getUserById(id)
-    }
-  /**
-   * 获取用户列表
-   * @returns
-   */
+import { ipcMain } from 'electron'
 
-  static async getList() {
-    return await userServices.getUserList()
+/**
+ * 获取用户列表
+ */
+ipcMain.handle('db/user/getList', async (event, arg) => {
+  return await userServices.getUserList()
+})
+
+/**
+ * 新增或更新用户
+ */
+ipcMain.handle('db/user/addOrUpdate', async (event, arg) => {
+  const data = arg
+  let res
+  if (data.id) {
+    res = await userServices.updateUserById(data.id, data)
+  } else {
+    const newData = { ...data }
+    delete newData.id
+    res = await userServices.insertUser(newData)
   }
-  /**
-   * 根据id删除用户
-   */
-  static async deleteById({id}) {
-    return await userServices.deleteUserById(id)
-  }
-}
+  return res
+})
+
+/**
+ * 根据用户id获取用户信息
+ */
+ipcMain.handle('db/user/getInfoById', async (event, { id }) => {
+  return await userServices.getUserById(id)
+})
+
+/**
+ * 根据用户id删除
+ */
+ipcMain.handle('db/user/deleteById', async (event, { id }) => {
+  return await userServices.deleteUserById(id)
+})
--- electron/main/dbInit.ts
+++ electron/main/dbInit.ts
@@ -1,6 +1,6 @@
 // 初始化用户表路由
-import {dbConnect}  from './db/dbConnect'
+import { dbConnect } from './db/dbConnect'
 export const dbInit = async () => {
-     await import('./router')
-     await dbConnect()
+  await import('./db/controller/index')
+  await dbConnect()
 }
